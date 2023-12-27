9fd6cc29b258354b6068fff7493e01a7c84d7a4e
Mini <60860894+minijaham@users.noreply.github.com>
2023-12-27 13:56:19 +0900

    Add files via upload

---
+++ src/Main.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace minijaham\ObsidianBreakerTNT;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\block\Water;
+use pocketmine\block\VanillaBlocks;
+
+use pocketmine\entity\object\PrimedTNT;
+
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityExplodeEvent;
+use pocketmine\event\block\BlockBreakEvent;
+
+class Main extends PluginBase implements Listener
+{
+    /** @var ObsidianData[] */
+    private array $obsidian = [];
+
+    public function onEnable() : void
+    {
+        $this->saveDefaultConfig();
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onEntityExplode(EntityExplodeEvent $event) : void
+    {
+        $entity = $event->getEntity();
+
+        if ($entity instanceof PrimedTNT) {
+            $pos    = $entity->getPosition();
+            $center = $entity->getWorld()->getBlockAt(
+                intval($pos->getFloorX()),
+                intval($pos->getFloorY()),
+                intval($pos->getFloorZ())
+            );
+
+            // Cancel TNT Explosion in water
+            if ($center instanceof Water) {
+                return;
+            }
+
+            $affected = [];
+
+            // Get blocks from all side of the PrimedTNT
+            for ($i = 0; $i <= 6; $i++) {
+                $affected[] = $center->getSide($i);
+            }
+
+            foreach ($affected as $block) {
+                // If block isn't obsidian
+                if(!$block instanceof (VanillaBlocks::OBSIDIAN())) {
+                    continue;
+                }
+
+                // If ObsidianData already exists, then add count if it does.
+                $found = false;
+                foreach ($this->obsidian as $obsidianData) {
+                    if ($obsidianData->getPosition()->equals($block->getPosition())) {
+                        $obsidianData->addCount();
+                        $found = true;
+                    }
+                }
+
+                // If ObsidianData doesn't exist, create a new one
+                if (!$found) {
+                    $this->obsidian[] = new ObsidianData($block);
+                }
+
+                $this->obsidian = array_filter($this->obsidian, function ($object) {
+                    if($object->getCount() >= $this->getConfig()->getNested("hit-count")) {
+                        $object->getPosition()->getWorld()->setBlockAt(
+                            intval($object->getPosition()->getX()),
+                            intval($object->getPosition()->getY()),
+                            intval($object->getPosition()->getZ()),
+                            VanillaBlocks::AIR()
+                        );
+                        return false; // returning false will remove the ObsidianData from the array
+                    }
+                    return true;
+                });
+            }
+        }
+    }
+
+    /**
+     * Remove ObsidianData if the obsidian is broken by hand
+     */
+    public function onBlockBreak(BlockBreakEvent $event) : void
+    {
+        $block = $event->getBlock();
+
+        $this->obsidian = array_filter($this->obsidian, (function ($object) use ($block) {
+            if($object->getPosition()->equals($block->getPosition())) {
+                return false; // returning false will remove the ObsidianData from the array
+            }
+            return true;
+        }));
+    }
+}
